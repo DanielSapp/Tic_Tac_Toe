b5bf68cee0978a44e0dc42b2b2f75c43ab08d70b
Enikitten <enipony@gmail.com>
2019-11-13 00:37:21 -0600

    Added method to update text when a empty space on the gameboard is clicked.

--- app/src/main/java/com/example/tictactoe/GameActivity.java
+++ app/src/main/java/com/example/tictactoe/GameActivity.java
@@ -3,12 +3,28 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GameActivity extends AppCompatActivity {
+    int currentPlayer = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+    }
+    public void onBoardMove(View v) {
+        Button clickedButton = (Button) v;
+        CharSequence cs = clickedButton.getText();
+        if (cs.length() != 0) {
+            return;
+        }
+        if (currentPlayer == 1) {
+            clickedButton.setText("X");
+        } else {
+            clickedButton.setText("O");
+        }
+        //TODO: CHECK IF WIN CONDITION HAS BEEN REACHED
     }
 }
