57f504ca17f594235ac2c018164257a3f7051831
Enikitten <enipony@gmail.com>
2019-11-13 01:08:55 -0600

    Added logic to keep a internal representation of the gameboard's state, methods to change the game's main text box and display a winning message when the game ends, a method to check for horzontal wins, and stubs for other win-checking methods

--- app/src/main/java/com/example/tictactoe/GameActivity.java
+++ app/src/main/java/com/example/tictactoe/GameActivity.java
@@ -5,14 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
-    int currentPlayer = 1;
+    char currentPlayer = 'X';
+    char[] gameState = new char[] {'e','e','e','e','e','e','e','e','e'};
+    TextView textDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        textDisplay = findViewById(R.id.GameTextDisplay);
     }
     public void onBoardMove(View v) {
         Button clickedButton = (Button) v;
@@ -25,6 +29,67 @@ public class GameActivity extends AppCompatActivity {
         } else {
             clickedButton.setText("O");
         }
-        //TODO: CHECK IF WIN CONDITION HAS BEEN REACHED
+        setGameState(clickedButton);
+        if (isHorizontalWin() || isVerticalWin() || isDiagonalWin()) {
+            displayWinMessage();
+            resetBoard();
+        }
+
+
+    }
+    private void setGameState(Button clickedButton) {
+        switch (clickedButton.getId()) {
+            case R.id.ULButton:
+                gameState[0] = currentPlayer;
+                break;
+            case R.id.UMButton:
+                gameState[1] = currentPlayer;
+                break;
+            case R.id.URButton:
+                gameState[2] = currentPlayer;
+                break;
+            case R.id.MLButton:
+                gameState[3] = currentPlayer;
+                break;
+            case R.id.MMButton:
+                gameState[4] = currentPlayer;
+                break;
+            case R.id.MRButton:
+                gameState[5] = currentPlayer;
+                break;
+            case R.id.BLButton:
+                gameState[6] = currentPlayer;
+                break;
+            case R.id.BMButton:
+                gameState[7] = currentPlayer;
+                break;
+            case R.id.BRButton:
+                gameState[8] = currentPlayer;
+        }
+    }
+    private boolean isHorizontalWin() {
+        for (int i = 0; i < gameState.length-2; i++) {
+            if (gameState[i] == currentPlayer && (gameState[i+1] == gameState[i] && gameState[i+2] == gameState[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private void setTextDisplay(String s) {
+        textDisplay.setText(s);
+    }
+    private void displayWinMessage() {
+        setTextDisplay("Congratulations player " + (currentPlayer == 'X' ? "1" : "2") + ", you won!");
+    }
+    private void resetBoard() {
+        //TODO
+    }
+    private boolean isVerticalWin() {
+        //TODO
+        return false;
+    }
+    private boolean isDiagonalWin() {
+        //TODO
+        return false;
     }
 }
